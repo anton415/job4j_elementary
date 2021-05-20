80315eedb4d5a9e25381a450bbe34996ba0effa8
Anton Serdyuchenko <anton415@gmail.com>
2021-05-20 19:24:59 +0300

    4.1. Аргументы [#295521 #158692]

--- src/ru/job4j/ArgMethod.java
+++ src/ru/job4j/ArgMethod.java
@@ -2,20 +2,15 @@ package ru.job4j;
 
 public class ArgMethod {
 
-    public static void hello(String name) {
+    public static void hello(String name, int age) {
         System.out.println("Hello, " + name);
+        System.out.println("Your age is " + age);
     }
 
     public static void main(String[] args) {
         String name = "Anton Serdyuchenko";
-        int age = 33;
+        int age = 27;
 
-        ArgMethod.hello(name);
-
-        ArgMethod.hello(name);
-
-        ArgMethod.hello(name);
-
-        ArgMethod.hello(name);
+        ArgMethod.hello(name, age);
     }
 }
