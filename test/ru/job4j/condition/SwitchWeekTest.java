e6949a511ce678e6e40d7cb1aba38236d148976d
Anton Serdyuchenko <anton415@gmail.com>
2021-05-22 15:27:26 +0300

    4.6.1. Switch [#334070 #159384]

--- test/ru/job4j/condition/SwitchWeekTest.java
+++ test/ru/job4j/condition/SwitchWeekTest.java
@@ -9,9 +9,7 @@ public class SwitchWeekTest {
     public void when1thenMonday() {
         int day = 1;
         String expected = "Monday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -19,9 +17,7 @@ public class SwitchWeekTest {
     public void when2thenTuesday() {
         int day = 2;
         String expected = "Tuesday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -29,9 +25,7 @@ public class SwitchWeekTest {
     public void when3thenWednesday() {
         int day = 3;
         String expected = "Wednesday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -39,9 +33,7 @@ public class SwitchWeekTest {
     public void when4thenThursday() {
         int day = 4;
         String expected = "Thursday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -49,9 +41,7 @@ public class SwitchWeekTest {
     public void when5thenFriday() {
         int day = 5;
         String expected = "Friday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -59,9 +49,7 @@ public class SwitchWeekTest {
     public void when6thenSaturday() {
         int day = 6;
         String expected = "Saturday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -69,9 +57,7 @@ public class SwitchWeekTest {
     public void when7thenSunday() {
         int day = 7;
         String expected = "Sunday";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
@@ -79,9 +65,7 @@ public class SwitchWeekTest {
     public void when0thenError() {
         int day = 0;
         String expected = "Error";
-
         String out = SwitchWeek.nameOfDay(day);
-
         Assert.assertEquals(expected, out);
     }
 
