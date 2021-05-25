9a01e18a53a0d46d73bd6f412e51671b48fe39a8
Anton Serdyuchenko <anton415@gmail.com>
2021-05-25 22:22:39 +0300

    5.7. Ипотека [#156315 #160548]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        percent = percent / 100 + 1;
         while (amount > 0) {
-            amount *= percent;
+            amount *= percent / 100 + 1;
             amount -= salary;
             year++;
         }
