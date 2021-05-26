4eba2f4ef4a466fa54fe9b5af0f4a5f6668e2785
Anton Serdyuchenko <anton415@gmail.com>
2021-05-26 16:55:09 +0300

    6.2. Упорядочить массив. [#4441 #160914]

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -1,25 +1,20 @@
 package ru.job4j.array;
 
 public class AlgoArray {
-    public static void bubbleSort(int[] array) {
-        boolean sorted = false;
-        int temp;
-        while (!sorted) {
-            sorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                    sorted = false;
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
-        bubbleSort(array);
+        int temp = array[0]; /* переменная для временного хранения значение ячейки с индексом 0. */
+        array[0] = array[3]; /* записываем в ячейку с индексом 0 значение ячейки с индексом 3. */
+        array[3] = temp; /* записываем в ячейку с индексом 3 значение временной переменной. */
+
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        temp = array[3];
+        array[3] = array[4];
+        array[4] = temp;
+
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
